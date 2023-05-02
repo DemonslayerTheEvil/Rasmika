51f2a3aff90a2739cde0a9a06891f33c2a819a94
DemonslayerTheEvil <130355084+DemonslayerTheEvil@users.noreply.github.com>
2023-05-02 06:54:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincessV2 /LazyPrincessV2
+  git clone https://github.com/DemonslayerTheEvil/Rasmika /LazyPrincessV2
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /LazyPrincessV2
 fi
-cd /LazyPrincessV2
+cd /Rasmika
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
